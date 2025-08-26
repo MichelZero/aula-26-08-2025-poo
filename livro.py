bc7c25625e38482d63bef849659d768ea65bc726
MichelZero <silva.michel@ifpb.edu.br>
2025-08-26 07:26:58 -0300

    up

---
+++ livro.py
@@ -0,0 +1,25 @@
+#
+# autor: Michel
+# data: 26/08/2025
+
+# 1. Definição da Classe Livro e seu Método ler()
+
+class Livro:
+    """
+    Representa um livro com um título e a capacidade de ser lido.
+    """
+    
+    def __init__(self, titulo):
+        """
+        Construtor da classe. É chamado quando um novo objeto Livro é criado.
+        
+        Parâmetros:
+            titulo (str): O título do livro.
+        """
+        self.titulo = titulo  # Atributo que armazena o título de cada livro
+
+    def ler(self):
+        """
+        Simula a ação de ler o livro, imprimindo uma mensagem com seu título.
+        """
+        print(f"Lendo {self.titulo}")
---
+++ main.py
@@ -0,0 +1,24 @@
+#
+# autor: Michel
+# data: 26/08/2025
+
+# importando a classe livro
+from livro import Livro
+
+# 2. Instanciando e Usando os Objetos
+
+# Cria o primeiro objeto (instância) da classe Livro
+livro_principe = Livro("O Pequeno Príncipe")
+
+# Cria o segundo objeto (instância) da classe Livro
+livro_casmurro = Livro("Dom Casmurro")
+
+
+# 3. Chamando o método ler() para cada objeto
+
+# Chama o método para o primeiro livro
+livro_principe.ler()
+
+# Chama o método para o segundo livro
+livro_casmurro.ler()
+
